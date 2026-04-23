c0c4682adbd9b7591025531379427a2769add863
imon134 <imonakpengbe@gmail.com>
2026-04-23 08:17:05 +0100

    Fix Vercel crash

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -1,51 +1,11 @@
 import os
 import dj_database_url
 
-DEBUG = False
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-ALLOWED_HOSTS = ["*"]
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set")
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL", "")
-    )
+    "default": dj_database_url.parse(DATABASE_URL)
 }
-
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-
-    "profiles",
-]
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-USE_TZ = True
-TIME_ZONE = "UTC"
