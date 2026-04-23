4ba5983f91be1d2211cbf28b58b226819afc222a
imon134 <imonakpengbe@gmail.com>
2026-04-23 08:43:43 +0100

    Production-ready fix

--- profile_project/urls.py
+++ profile_project/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"status": "API is live"})
 
 urlpatterns = [
+    path("", home),
     path("api/", include("profiles.urls")),
 ]
